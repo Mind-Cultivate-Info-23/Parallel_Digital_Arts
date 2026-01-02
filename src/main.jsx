28671591655f5aa09f7c2cdfb43598fcc802c49e
Ray <mindcultivateinfo@gmail.com>
2026-01-03 01:55:24 +0200

    Teams page coming through

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import Home from './Home.jsx'
 import Courses from './Courses.jsx'
+import Team from './Team.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Home />
     <h1>Dont worry we'll fix this</h1>
     <Courses />
+    <h1> This too or two?</h1>
+    <Team />
   </StrictMode>,
 )
